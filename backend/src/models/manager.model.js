ba5ea97c47290dfce939403de340cbda770a83b6
himanshu <sharmahimansh276@gmail.com>
2022-04-17 12:52:43 +0530

    hashed the possword

--- backend/src/models/manager.model.js
+++ backend/src/models/manager.model.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcryptjs")
 
 const mongoose = require("mongoose")
 
+// creating the manager schema 
+
 const managerSchema = new mongoose.Schema({
     name:{type:String, required:true},
     password:{type:String, required:true},
@@ -12,3 +14,20 @@ const managerSchema = new mongoose.Schema({
     timestamps:true,
     versionKey:false
 })
+
+// hasing the password
+
+mongooseSchema.pre("save", function(next){
+    if(!this.ismodified("password")){
+        return next();
+    };
+
+    const hash = bcrypt.hashSync(this.password, 8);
+    this.password = hash
+})
+
+managerSchema.maethods.comparePassword = function(userPassword){
+    return bcrypt.compareSync(userPassword, this.password);
+}
+
+module.exports = mongoose.model("manager", managerSchema);
